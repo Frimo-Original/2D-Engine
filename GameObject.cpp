e539570f03780fb698d1aeeb7333d1d40555b2f6
Kucherenko-Michael <57537925+Kucherenko-Michael@users.noreply.github.com>
2020-03-06 17:17:18 +0300

    Add files via upload

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,6 +1,7 @@
 #include "GameObject.h"
 
-Engine::GameObject::GameObject(GameScene* scene, Textures* textures) : scene(scene), textures(textures) { }
+Engine::GameObject::GameObject(GameScene* scene, Textures* textures, Vector2i size) :
+	scene(scene), textures(textures), size(size) { }
 
 Engine::GameObject::~GameObject() {
 	delete textures;
@@ -17,4 +18,28 @@ void Engine::GameObject::setTexture(int number) {
 
 void Engine::GameObject::draw(sf::RenderWindow* window) {
 	window->draw(sf::Sprite(*texture));
+}
+
+void Engine::GameObject::setSize(Vector2i size) {
+	this->size = size;
+}
+
+Engine::Vector2i Engine::GameObject::getSize() {
+	return size;
+}
+
+void Engine::GameObject::setPosition(Engine::Vector2i position) {
+	this->position = position;
+}
+
+Engine::Vector2i Engine::GameObject::getPosition() {
+	return position;
+}
+
+void Engine::GameObject::setSpeed(Engine::Vector2f speed) {
+	this->speed = speed;
+}
+
+Engine::Vector2f Engine::GameObject::getSpeed() {
+	return speed;
 }
